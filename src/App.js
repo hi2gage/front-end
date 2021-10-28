e2e78f41c0be446f43ded0d3026810b67cf97b67
Gage Halverson <gagehalverson@iMac.lan>
2021-10-28 13:20:37 -0600

    working front end

--- src/App.js
+++ src/App.js
@@ -3,28 +3,20 @@ import './App.css';
 
 
 function App() {
-  // const [data, setData] = React.useState(null);
+  const [data, setData] = React.useState(null);
 
   React.useEffect(() => {
-    fetch('https://csci-331-snow-project.herokuapp.com/api/')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(response.statusText)
-        }
-        return response.json()
-      }).catch(err=>{
-      console.log(err)
-  })}, []);
-
-  
+    fetch("https://csci-331-snow-project.herokuapp.com/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
 
   return (
-    <body>
+    <div>
       <h2>test</h2>
-      {/* <h3>{data}</h3> */}
+      <h3>{data}</h3>
       <h2>test</h2>
-
-    </body>
+    </div>  
   );
 }
 
