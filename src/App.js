2742bcc358f70038ddd1f69cddc172e337d5ab28
Gage Halverson <gagehalverson@iMac.lan>
2021-10-28 12:53:18 -0600

    fixed

--- src/App.js
+++ src/App.js
@@ -3,19 +3,26 @@ import './App.css';
 
 
 function App() {
-  const [data, setData] = React.useState(null);
-
+  // const [data, setData] = React.useState(null);
 
   React.useEffect(() => {
-    fetch("https://csci-331-snow-project.herokuapp.com/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.data));
-  }, []);
+    fetch('https://csci-331-snow-project.herokuapp.com/api/')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(response.statusText)
+        }
+        return response.json()
+      }).catch(err=>{
+      console.log(err)
+  })}, []);
+
+  
 
   return (
     <body>
       <h2>test</h2>
-      <h3>Before +{data} After</h3>
+      {/* <h3>{data}</h3> */}
+      <h2>test</h2>
 
     </body>
   );
