4c572915c6b0c52c593069d7440efb433ea6abc4
Gage Halverson <gagehalverson@iMac.lan>
2021-10-28 02:32:54 -0600

    fixed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
 import React from 'react'
 import './App.css';
-import { useState } from 'react';
+
 
 function App() {
   const [data, setData] = React.useState(null);
-  const [value, onChange] = useState('10:00');
+
 
   React.useEffect(() => {
     fetch("https://csci-331-snow-project.herokuapp.com/api")
       .then((res) => res.json())
-      .then((data) => setData(data.message));
+      .then((data) => setData(data.data));
   }, []);
 
   return (
-    <body class="bg-gray-200 bg-h-min text-center min-h-screen">
+    <body>
+      <h2>test</h2>
+      <h3>Before +{data} After</h3>
 
-      <div class="flex h-screen ">
-        <div class="m-auto text-grey-1000 font-">
-          <h3>{data}</h3>
-        </div>
-      </div>
     </body>
-    
   );
 }
 
