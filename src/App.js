57719d05c37cff4c412470ea4b30accbc622bb8e
hi2gage <hi2gage.pdx@gmail.com>
2021-11-30 21:07:12 -0700

    changing run

--- src/App.js
+++ src/App.js
@@ -8,24 +8,28 @@ import axios from 'axios';
 function App() {
     const [data, setData] = React.useState(null);
 
-    const getFromApi = () => {
-        axios.get('https://csci-331-snow-project.herokuapp.com/apidb')
-            .then(response => {
-                console.log(JSON.stringify(response.data, null, " "));
-                const info = response.data;
-                setData(info);
-            })
-            .catch(error => console.error(error));
-    }
+    React.useEffect(() => {
+        getFromApi = () => {
+            axios.get('https://csci-331-snow-project.herokuapp.com/apidb')
+                .then(response => {
+                    console.log(JSON.stringify(response.data, null, " "));
+                    const info = response.data;
+                    setData(info);
+                })
+                .catch(error => console.error(error));
+        }
+    }, []);
 
-    React.useEffect(() => getFromApi(), []);
+
+    if (!data) return null;
 
     return (
         <div>
-            <h2>test</h2>
-            {/* <h3>{ data[0].snow }</h3> */}
-            <h2>test</h2>
-            <button onClick={getFromApi()}> this is a button</button>
+            <h2>test start</h2>
+            <h3>{data[0].hr}</h3>
+            <h3>{data[0].min}</h3>
+            <h3>{data[0].snow}</h3>
+            <h2>test end</h2>
         </div>
     );
 }
