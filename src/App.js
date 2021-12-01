6a44be8c1db3e3eb0d091a4faa3ad20d587ba793
hi2gage <hi2gage.pdx@gmail.com>
2021-11-30 19:57:10 -0700

    fixed the errors in the fetch

--- src/App.js
+++ src/App.js
@@ -3,20 +3,29 @@ import './App.css';
 
 
 function App() {
-  const [data, setData] = React.useState(null);
+    const [data, setData] = React.useState(null);
 
-  React.useEffect(() => {
-    fetch("https://csci-331-snow-project.herokuapp.com/api")
-      .then(setData(res.rows));
-  }, []);
+    React.useEffect(() => {
+        fetch('https://csci-331-snow-project.herokuapp.com/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(credentials)
+        })
+            .then(returnedData => {
+                setData(returnedData)
 
-  return (
-    <div>
-      <h2>test</h2>
-      <h3>{data}</h3>
-      <h2>test</h2>
-    </div>  
-  );
+            })
+    }, []);
+
+    return (
+        <div>
+            <h2>test</h2>
+            <h3>{data}</h3>
+            <h2>test</h2>
+        </div>
+    );
 }
 
 export default App;
